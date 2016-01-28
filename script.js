ce63ebdfa1f7ee7b632c0108cdb83a99b97380ca
lianaschapiro <liana.schapiro@gmail.com>
2016-01-28 18:03:04 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+$.stellar();
+
+// $(document).ready(function(){
+// $('nav').hide();
+	// $(window).scroll(function(){
+ //        if ($(this).scrollTop() > 200) {
+ //            $('nav').fadeIn(650),
+ //            // $('#introText').fadeOut(400)
+ //        } else {
+ //            $('nav').fadeOut(650),
+ //            // $('#introText').fadeIn(400);
+ //        }
+ //    });
