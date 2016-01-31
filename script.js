dd9b10c991b67eb4adc7d95f1d2fb720d0454f58
lianaschapiro <liana.schapiro@gmail.com>
2016-01-30 21:10:06 -0500

    Added animation to contact information

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
+
 $(document).ready(function(){
+	$("#contact-content").css({
+	    		'display' : "none"
+	    		}); 
+	
+	if (($('#about').offset().top - $(window).scrollTop()) < 400) {
+		$("#about").css({
+	    		'display' : "none"
+	    		});
+	};
 
-
-	// Fades out my name when page is scrolled
+	
 	$(window).scroll(function() {
+		// Fades my name out/in when page is scrolled
+		console.log($('#about').offset().top - $(window).scrollTop());
 	    $("#headline").css({
-	    'opacity' : 1-(($(this).scrollTop())/300)
-	    });         
-	});  
-  
+	    	'opacity' : 1-(($(this).scrollTop())/300)
+	    }); 
+
+	    
+	    // Fades contact information in/out when page is scrolled
+		// if($(window).scrollTop() + $(window).height() > $(document).height() - ($(document).height() * .08)) {
+     	if (($('#contact').offset().top - $(window).scrollTop()) < 220) {
+			$("#contact-content").fadeIn();
+	    }
+	    else {
+	    	$("#contact-content").fadeOut();
+	    };
+	});
+
 
 });
 
